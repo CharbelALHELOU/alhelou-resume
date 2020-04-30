346743d1d37a54aeca8a7fbb5be61e091ac72c86
Ging <charbel.alhelou@ensta-paris.fr>
2020-04-30 21:26:01 +0200

    added the components and the typewriter effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,26 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { PortofolioComponent } from './portofolio/portofolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TypewriterComponent } from './typewriter/typewriter.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PortofolioComponent,
+    AboutComponent,
+    ContactComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    TypewriterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/typewriter/typewriter.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-typewriter',
+  templateUrl: './typewriter.component.html',
+  styleUrls: ['./typewriter.component.css']
+})
+export class TypewriterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+      this.typingCallback(this);
+    
+  }
+
+  @Input() message: string;
+  typewriter_display: string = "";
+
+  typingCallback(that) {
+    let total_length = that.message.length;
+    let current_length = that.typewriter_display.length;
+    if (current_length < total_length) {
+      that.typewriter_display += that.message[current_length];
+    } else {  }
+    setTimeout(that.typingCallback, 100, that);
+  }
+}
